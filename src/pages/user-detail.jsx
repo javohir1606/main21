0383a3c389fd5a11ac1bc82ffd828cafa437cff1
javohir1606 <javohirallayorov1606@gmail.com>
2024-11-05 21:54:46 +0500

    main

--- src/pages/user-detail.jsx
+++ src/pages/user-detail.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import { useGetUserByIdQuery } from "../components/show/showCard";
 
 export function UserDetail() {
-  const { id } = useParams();
-  const { data: userData, error, isLoading } = useGetUserByIdQuery(id); 
-
-  if (isLoading) return <p>Loading...</p>; 
-  if (error) return <p>Error fetching user data.</p>; 
-  if (!userData) return <p>User not found.</p>; 
 
   return (
     <div>
-      <Typography variant="h3">{userData.title}</Typography>
-      <Typography variant="h5">{userData.description}</Typography>
+      <h1>Lorem ipsum dolor sit.</h1>
     </div>
   );
 }
